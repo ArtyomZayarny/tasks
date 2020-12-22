ab2d78a2ed2e2af34aeed410107e179241ab7961
Artem Zayarny <Timaz@Artems-MacBook-Pro.local>
2020-12-22 07:01:12 +0200

    Setup project

---
+++ back/src/tasks/tasks.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { Task } from './task.model'
+
+@Injectable()
+export class TasksService {
+    private tasks = []
+
+    getAllTasks(): Task[] {
+        return this.tasks
+    }
+}
